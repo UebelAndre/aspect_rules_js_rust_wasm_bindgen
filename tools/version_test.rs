03ee812596486f1c2156a213e309455bcede2ede
UebelAndre <github@uebelandre.com>
2026-03-02 18:55:26 -0800

    Additional renames

--- tools/version_test.rs
+++ tools/version_test.rs
@@ -35,6 +35,6 @@ fn module_bzl_has_correct_version() {
 
     assert_eq!(
         version, module_bazel_version,
-        "@rules_js_rust_wasm_bindgen//:version.bzl and //:MODULE.bazel versions are out of sync"
+        "@aspect_rules_js_rust_wasm_bindgen//:version.bzl and //:MODULE.bazel versions are out of sync"
     );
 }
